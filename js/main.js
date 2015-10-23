876c67cb73a3c647a95c5e585d0db524f848652e
Graphic <graphic@SOD-SJH-516-22M.local>
2015-10-23 14:53:38 -0400

    end of class

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(window).on('resize', function () {
+
+	var w = $('.hills').width();
+	var h = $('.hills').height();
+
+	$('.hills').css('transform');
+
+});
+
+
+
+
+
+
+// $(window).on('scroll',function() {
+
+// 	var px = $(window).scrollTop();
+// 	console.log(px);
+
+// });
+
